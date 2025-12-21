bce73efd9771b36bda08e4feb7ab84a15e9352b5
Beyza Korkmaz <116458658+beyzakrkmzz123@users.noreply.github.com>
2025-12-21 23:39:48 +0300

    fix auth middleware options

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,6 +2,11 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 export default async function authMiddleware(req, res, next) {
+  // 🔓 CORS preflight (OPTIONS) isteğine izin ver
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
